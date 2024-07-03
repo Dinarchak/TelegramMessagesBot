ea3d338e22a3fc09b178e181b53ba592e9cd22a9
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-03 17:07:51 +0300

    Вынес кнопку для запроса чата в отдельную клавиатуру

--- keyboards.py
+++ keyboards.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton,  KeyboardButtonRequestChat
 
 start_kb = ReplyKeyboardMarkup(
     keyboard=[
@@ -7,12 +7,35 @@ start_kb = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
+select_chat_kb = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='Выберите группу',
+                           request_chat=KeyboardButtonRequestChat(request_id=1,
+                                                                  chat_is_channel=False,
+                                                                  chat_is_forum=False)
+                           )
+        ]
+    ],
+    resize_keyboard=True
+)
+
 filters_kb = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='Текст'), KeyboardButton(text='Пользователь')],
         [KeyboardButton(text='Даты'), KeyboardButton(text='Хештеги')],
-        [KeyboardButton(text='Чат'), KeyboardButton(text='...')],
+        [KeyboardButton(text='...')],
         [KeyboardButton(text='Найти')],
+        [KeyboardButton(text='Назад')],
+    ],
+    resize_keyboard=True
+)
+
+additional_filters = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text='Изображение'), KeyboardButton(text='Файл'), KeyboardButton(text='Ссылка')],
+            [KeyboardButton(text='Найти')],
+        [KeyboardButton(text='Назад')],
     ],
     resize_keyboard=True
 )
