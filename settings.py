bd46c7e53b99f23d54cb75f5b83dad3b424bf945
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-03 10:14:21 +0300

    Конфиг и настройки

---
+++ settings.py
@@ -0,0 +1,12 @@
+import yaml
+import os
+
+def parent(path):
+    return os.path.abspath(os.path.join(path, os.pardir))
+
+BASE_DIR = parent(__file__)
+
+CONFIG_PATH = BASE_DIR + '\\config.yaml'
+
+with open(CONFIG_PATH) as f:
+    config = yaml.safe_load(f)
