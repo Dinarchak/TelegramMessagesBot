6295f7391387c6c2ccec86dda393f602165e853e
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-03 11:25:37 +0300

    Избаился от явного соединения и подключил начальную клавиатуру

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command, CommandStart
 from settings import config
-
 from aiogram.types import Message
-from tortoise import Tortoise, connections
 
+from tortoise import Tortoise
 from models import *
+
+from keyboards import start_kb, filters_kb
 
 import asyncio
 
 bot = Bot(token=config['token'])
 dp = Dispatcher()
-conn = None
+
 
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer('Привет долбаеб')
+    await message.answer('Привет долбаеб', reply_markup=start_kb)
+
+
+@dp.message(F.text.lower() == 'найти')
+async def listen_filters(message: Message):
+    await message.answer('Укажите фильтры для поиска', reply_markup=filters_kb)
+
+
 
 @dp.message()
 async def foo(message: Message):
-    global conn
-
-    await message.answer(f'{message.from_user.first_name} {message.from_user.last_name} {message.text}')
-    user = await User.get_or_create(first_name=message.from_user.first_name, last_name=message.from_user.last_name, using_db=conn)
-    await Message.create(sender=user[0], text=message.text, date=message.date, using_db=conn)
+    user = await User.get_or_create(first_name=message.from_user.first_name, last_name=message.from_user.last_name)
+    await Message.create(sender=user[0], text=message.text, date=message.date)
 
 
 async def main():
-    # удалить вебхуки и перейти на пулинг и общение с пользователем через getUpdates
-    global conn
-
     await Tortoise.init(
             db_url=config['db_url'],
             modules={'user': ['models']}
             )
 
-    conn = connections.get('default')
-    print(conn)
-
     # await Tortoise.generate_schemas()
 
+    # удалить вебхуки и перейти на пулинг и общение с пользователем через getUpdates
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-    print('Бот запущен')
 
 if __name__ == '__main__':
     asyncio.run(main())
