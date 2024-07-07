cf4e54d5a301f01023e2f33cbb79b61fef952bba
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-07 19:31:34 +0300

    Костыль короче

--- keyboards/filters_kb.py
+++ keyboards/filters_kb.py
@@ -5,7 +5,7 @@ filters = ReplyKeyboardMarkup(
         [KeyboardButton(text='Текст'), KeyboardButton(text='Пользователь')],
         [KeyboardButton(text='Даты'), KeyboardButton(text='Хештеги')],
         [KeyboardButton(text='...')],
-        [KeyboardButton(text='Найти')],
+        [KeyboardButton(text='Найти сообщения')],
         [KeyboardButton(text='Назад')],
     ],
     resize_keyboard=True
