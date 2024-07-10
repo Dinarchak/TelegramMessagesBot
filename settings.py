eb46d642d0be4e4d0f15b86720fd8a3543507048
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-10 21:09:45 +0300

    Вынес объект бота из главного файла в файл настроек, чтобы импортировать в другие модули

--- settings.py
+++ settings.py
@@ -1,8 +1,11 @@
 import yaml
 import os
+from aiogram import Bot
+
 
 def parent(path):
     return os.path.abspath(os.path.join(path, os.pardir))
+
 
 BASE_DIR = parent(__file__)
 
@@ -10,3 +13,6 @@ CONFIG_PATH = BASE_DIR + '\\config.yaml'
 
 with open(CONFIG_PATH) as f:
     config = yaml.safe_load(f)
+
+bot = Bot(token=config['token'])
+
