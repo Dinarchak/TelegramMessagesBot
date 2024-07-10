e7892b370544f8eaa7e0bb7ed368b44ff6767920
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-10 21:14:06 +0300

    Новый роутер

--- handlers/find_messages.py
+++ handlers/find_messages.py
@@ -1,15 +1,10 @@
 from aiogram import Router, F
 from aiogram import types as atp
 from aiogram.fsm.context import FSMContext
+from models import Message, Hashtag, User, Chat
+from settings import bot
+from states import Form
 
+from tortoise.expressions import Q
 
 find_messages_router = Router(name='find_messages')
-
-
-@find_messages_router.message(F.text.lower() == 'найти сообщения')
-async def find_messages(message: atp.Message, state: FSMContext):
-    data = await state.get_data()
-    print(data)
-    await state.clear()
-
-
