158dd1e77311b9cf872379cad25285379f3c97f7
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-17 11:40:38 +0300

    Кнопку Найти заменить на кнопку Найти сообщения

--- keyboards/additional_filters_kb.py
+++ keyboards/additional_filters_kb.py
@@ -3,7 +3,7 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 additional_filters = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text='Изображение'), KeyboardButton(text='Файл'), KeyboardButton(text='Ссылка')],
-            [KeyboardButton(text='Найти')],
+            [KeyboardButton(text='Найти сообщения')],
         [KeyboardButton(text='Назад')],
     ],
     resize_keyboard=True
