63510fa8fa291e3ca9f4045e9484d0b8378b5cdb
Dinar Fazliev <jjustdinar@gmail.com>
2024-07-17 11:41:08 +0300

    У юзера может и не быть Имени и Фамилии

--- models/user.py
+++ models/user.py
@@ -5,6 +5,6 @@ from tortoise import fields
 class User(Model):
     id = fields.IntField(primary_key=True)
     user_id = fields.IntField(unique=True)
-    first_name = fields.CharField(max_length=255)
-    last_name = fields.CharField(max_length=255)
+    first_name = fields.CharField(max_length=255, null=True)
+    last_name = fields.CharField(max_length=255, null=True)
     username = fields.CharField(max_length=255)
